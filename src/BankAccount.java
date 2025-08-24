1f455bd21eeeabf19f2ef72c9fb90605ac0d0efd
SheikhGalib <galibmahim01@gmail.com>
2025-08-24 16:30:19 +0600

    Added bankAccount.java

---
+++ src/BankAccount.java
@@ -0,0 +1,27 @@
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+    
+    public BankAccount(String accountNumber, String accountHolderName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+    
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+    
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+    
+    public double checkBalance() {
+        return balance;
+    }
+}
